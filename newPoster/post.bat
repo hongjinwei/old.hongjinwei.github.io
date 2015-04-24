cbdcb119e654c1798d433a420e0ec6223dc7cdc7
Jerry Hong <hongjinwei222@gmail.com>
2015-04-24 17:33:25 +0800

    new post

--- newPoster/post.bat
+++ newPoster/post.bat
@@ -2,9 +2,9 @@ del main.json
 del backup\main.json
 
 xcopy ..\main.json .
-del ..\main.json
-xcopy ..\main.json backup\
+xcopy ..\main.json .\backup\
 
 python poster.py
 
+del ..\main.json
 xcopy main.json ..\
